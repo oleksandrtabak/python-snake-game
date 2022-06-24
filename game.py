ad66376be5690334305c941c389fa45c11828ff1
Олександр <oleksandrtabak@protonmail.com>
2022-06-24 17:49:41 +0300

    Дороблено інший функціонал

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import random
 # Змінні, типу константи. Не будуть змінюватись
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
-SPEED = 50
+SPEED = 100
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = '#00FF00'
@@ -13,7 +13,18 @@ BG_COLOR = '#000000'
 
 
 class Snake:
-    pass
+
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag='snake')
+            self.squares.append(square)
 
 class Food:
 
@@ -26,18 +37,79 @@ class Food:
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag='food')
 
     
-def next_turn():
-    pass
+def next_turn(snake, food):
+    x, y = snake.coordinates[0]
+
+    if direction == 'up':
+        y -= SPACE_SIZE
+    elif direction == 'down':
+        y += SPACE_SIZE
+    elif direction == 'left':
+        x -= SPACE_SIZE
+    elif direction == 'right':
+        x += SPACE_SIZE
+
+    snake.coordinates.insert(0, (x, y))
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+    snake.squares.insert(0, square)
+
+    # Коли з'їли їжу
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        global score
+        score += 1
+        label.config(text='Score:{}'.format(score))
+        canvas.delete('food')
+        food = Food()
+    else:
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+        del snake.squares[-1]
+
+    # Якщо є зіткнення
+    if check_collisions(snake):
+        game_over()
+    else:
+        window.after(SPEED, next_turn, snake, food)
+
 
 def change_direction(new_direction):
-    pass
+    global direction
 
-def check_collisions():
-    pass
+    if new_direction == 'left':
+        if direction != 'right': # Для того, щоб не розвертатись на 180
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
+
+
+def check_collisions(snake):
+    x, y = snake.coordinates[0]
+
+    # Якщо виходить за межі вікна
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_WIDTH:
+        return True
+
+    # Якщо врізається сам в себе
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
+
 
 def game_over():
-    pass
-
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width() / 2, canvas.winfo_height() / 2,
+                       font=('consolas', 70), text='Кінець', fill='red', tag='game')
 
 window = Tk()
 window.title('Змійка')
@@ -66,11 +138,18 @@ y = int((screen_height / 2) - (window_height / 2))
 window.geometry(f'{window_width}x{window_height}+{x}+{y}')
 
 
+#
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
 # Створення змійки
 snake = Snake()
 
 # Створення їжі
 food = Food()
 
+next_turn(snake, food)
 
 window.mainloop()
