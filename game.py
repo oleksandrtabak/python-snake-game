70963842bc49b878d8d1401a3f5ef755a3ae7ccf
Олександр <oleksandrtabak@protonmail.com>
2022-06-24 16:37:13 +0300

    Основні конструкції та константи

---
+++ game.py
@@ -0,0 +1,31 @@
+import tkinter
+import random
+
+# Змінні, типу константи. Не будуть змінюватись
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = '#00FF00'
+FOOD_COLOR = '#FF0000'
+BG_COLOR = '#000000'
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
